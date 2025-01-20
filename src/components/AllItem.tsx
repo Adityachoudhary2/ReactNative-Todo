499919e21e32c4a86393eb44bac9ee875f6dbf36
Aditya <choudharyaditya9381@gmail.com>
2025-01-20 16:49:28 +0530

    AllItem page Create

--- src/components/AllItem.tsx
+++ src/components/AllItem.tsx
@@ -1,14 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {FlatList, StyleSheet, Text, View } from 'react-native'
 
-const AllItem = () => {
+
+interface Item{
+  id: number;
+  name: string;
+  stock: number;
+}
+
+interface AllItemsProps {
+  data: Item[];
+}
+
+const AllItem: React.FC<AllItemsProps> = ({data}) => {
   return (
     <View>
-      <Text>AllItem</Text>
+     <View style={styles.headingContainer}>
+        <Text style={styles.headingtext}>Items</Text>
+        <Text style={styles.headingtext}>Quantity</Text>
+      </View>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={[
+              styles.itemContainer,
+              { backgroundColor: item.stock < 10 ? "#FFCCCC" : "#D7F6BFFF" },
+            ]}
+          >
+            <Text style={styles.itemtext}>{item.name}</Text>
+            <Text style={styles.itemtext}>{item.stock}</Text>
+          </View>
+        )}
+        contentContainerStyle={{ gap: 10 }}
+      />
     </View>
   )
 }
 
 export default AllItem
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  headingContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  headingtext: {
+    fontWeight: "500",
+    fontSize: 16,
+  },
+  itemContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 7,
+  },
+  itemtext: {
+    fontWeight: "400",
+    fontSize: 15,
+  },
+})
--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -3,6 +3,24 @@ import { Pressable, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import AllItem from './AllItem';
 import CreateScreen from './CreateScreen';
 
+//Define Data Item Type
+interface Item{
+  id:number;
+  name:string;
+  stock:number;
+
+
+}
+
+
+//Data array With Define Type 
+const data: Item[] = [
+  { id: 1, name: "English", stock: 5 },
+  { id: 2, name: "Science", stock: 15, },
+  { id: 3, name: "Mathematics", stock: 25},
+  { id: 4, name: "Chemistry", stock: 50},
+  { id: 5, name: "Physics", stock: 19},
+];
 
 
 const HomeScreen: React.FC = () => {
@@ -11,22 +29,22 @@ const HomeScreen: React.FC = () => {
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Deshboard</Text>
       <View style={styles.buttonContainer}>
-        <Pressable style={[styles.button, view === 0 ? { backgroundColor: 'green' } : null]}
+        <Pressable style={[styles.button, view === 0 ? { backgroundColor: '#72C37AFF' } : null]}
           onPress={() => setView(0)}>
           <Text style={[styles.btnText, view === 0 ? { color: 'white' } : null]}>All Item</Text>
         </Pressable>
-        <Pressable style={[styles.button, view === 1 ? { backgroundColor: 'green' } : null]}
+        <Pressable style={[styles.button, view === 1 ? { backgroundColor: '#72C37AFF' } : null]}
           onPress={() => setView(1)}>
           <Text style={[styles.btnText, view === 1 ? { color: 'white' } : null]}>Low Stock</Text>
         </Pressable>
-        <Pressable style={[styles.button, view === 2 ? { backgroundColor: 'green' } : null]}
+        <Pressable style={[styles.button, view === 2 ? { backgroundColor: '#72C37AFF' } : null]}
           onPress={() => setView(2)}>
           <Text style={[styles.btnText, view === 2 ? { color: 'white' } : null]}>Create</Text>
         </Pressable>
       </View>
 
-      {view === 0 && <AllItem />}
-      {view === 1 && <AllItem />}
+      {view === 0 && <AllItem  data={data}/>}
+      {/* {view === 1 && <AllItem />} */}
       {view === 2 && <CreateScreen />}
     </SafeAreaView>
   );
@@ -36,6 +54,7 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: "9%",
     width: '100%',
     height: '100%',
     padding: '4%',
@@ -56,7 +75,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     borderRadius: 50,
     borderWidth: 0.8,
-    borderColor: 'green',
+    borderColor: '#72C37AFF',
   },
   btnText: {
     color: 'green',
